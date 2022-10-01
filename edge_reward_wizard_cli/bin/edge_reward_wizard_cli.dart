557d62a221f9a3bfaafc3416e68a8896b7153d6c
CodeDevilME <codedevilme@gmail.com>
2022-10-01 20:11:07 +0530

    Cleaning up

--- edge_reward_wizard_cli/bin/edge_reward_wizard_cli.dart
+++
@@ -1,5 +0,0 @@
-import 'package:edge_reward_wizard_cli/edge_reward_wizard_cli.dart' as edge_reward_wizard_cli;
-
-void main(List<String> arguments) {
-  print('Hello world: ${edge_reward_wizard_cli.calculate()}!');
-}
--- edge_reward_wizard_cli/test/edge_reward_wizard_cli_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:edge_reward_wizard_cli/edge_reward_wizard_cli.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
