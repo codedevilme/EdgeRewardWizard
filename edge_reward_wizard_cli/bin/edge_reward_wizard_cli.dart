48196fb33228d5991ebb32c10cec11b3a9de171b
CodeDevilME <codedevilme@gmail.com>
2022-10-01 20:06:15 +0530

    CLI app files created

---
+++ edge_reward_wizard_cli/bin/edge_reward_wizard_cli.dart
@@ -0,0 +1,5 @@
+import 'package:edge_reward_wizard_cli/edge_reward_wizard_cli.dart' as edge_reward_wizard_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${edge_reward_wizard_cli.calculate()}!');
+}
---
+++ edge_reward_wizard_cli/test/edge_reward_wizard_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:edge_reward_wizard_cli/edge_reward_wizard_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
